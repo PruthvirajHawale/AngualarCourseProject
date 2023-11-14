0aa9092e46a314872fd4c5044984b0b1500f0efc
Pruthviraj Hawale <pruthviraj.hawale@netcracker.com>
2023-11-14 14:51:49 +0530

    Modularized application by creating recipe module , how the fuck autologin got fixed

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { AppRoutingModule } from "../app-routing.module";
+import { CommonModule } from "@angular/common";
+import { RecipesRoutingModule } from "./recipe-routing.module";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports: [AppRoutingModule, CommonModule, RecipesRoutingModule],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+})
+export class RecipeModule {}
